8b89b5ded56f43a607384be1dd45c4f615d8de72
harshal <harshal2030@outlook.com>
2020-02-08 17:30:39 +0530

    added partials and templates to weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,44 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
 
 const app = express();
 
+// define paths for express
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup HandleBars
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather App",
+        name: "Harshal Sanghvi"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: "Harshal Sanghvi"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help Page",
+        name: "Harshal Sanghvi"
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
