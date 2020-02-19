0b421d427357151e294668e9391546dcbce17d0d
harshal <harshal2030@outlook.com>
2020-02-19 20:35:33 +0530

    integrated async await with api endpoints

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,22 @@
 require('./../src/db/mongoose');
 const Task = require('./../src/models/tasks')
 
-Task.findByIdAndRemove('5e46dedb634c80219f9a16c4').then((res) => {
-    console.log(res)
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
-}).catch((e) => {
-    console.log(e)
-})
+// Task.findByIdAndRemove('5e46dedb634c80219f9a16c4').then((res) => {
+//     console.log(res)
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const DeleteAndCount = async (id) => {
+    const del = await Task.findByIdAndDelete('5e47d04186b828187a8b1c30')
+    const count = await Task.countDocuments({completed: false});
+
+    return count
+}
+
+DeleteAndCount('5e47d04186b828187a8b1c30').then((count) => {
+    console.log(count)
+}).catch((e) => console.log(e))
