ad7e5daa9a1c774ad357eb4e003fa2b109796906
harshal <harshal2030@outlook.com>
2020-02-22 22:08:14 +0530

    added middleware in express and bearer authorization

--- task-manager/src/router/user.js
+++ task-manager/src/router/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("./../models/user");
+const auth = require('./../middleware/auth');
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -27,13 +28,34 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
   try {
-    const user = await User.find({});
-    res.send(user);
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+
+    await req.user.save()
+
+    res.send()
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+
+    await req.user.save();
+
+    res.send()
   } catch (e) {
     res.status(500).send();
   }
+})
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
