e9f55db7f64502acb75cd8fc0bd326ac9f528a54
harshal <harshal2030@outlook.com>
2020-02-27 21:54:14 +0530

    learned to add file uploads to some extent

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,16 @@ const taskRouter = require('./router/task');
 const app = express();
 const port = process.env.PORT || 3000
 
+
+const multer = require('multer');
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter);
--- task-manager/src/router/user.js
+++ task-manager/src/router/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("./../models/user");
 const auth = require('./../middleware/auth');
 const router = new express.Router();
+const multer = require('multer');
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -90,5 +91,13 @@ router.delete("/users/me", auth, async (req, res) => {
     res.status(500).send();
   }
 });
+
+const upload = multer({
+  dest: 'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send();
+})
 
 module.exports = router;
